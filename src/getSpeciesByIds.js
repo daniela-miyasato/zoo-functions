5373c25cbfd1bd3c1a488f4b0f7f757e4d96b63e
Daniela Miyasato Goto <daniela.miyasato@live.com>
2022-01-25 16:34:32 -0300

    Requisito 1 - getSpeciesByIds

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  return ids.map((item) => data.species.find((element) => element.id === item));
 }
 
+// console.log(getSpeciesByIds('89be95b3-47e4-4c5b-b687-1fabf2afa274', '533bebf3-6bbe-41d8-9cdf-46f7d13b62ae'));
 module.exports = getSpeciesByIds;
