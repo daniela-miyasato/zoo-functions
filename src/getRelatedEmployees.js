d39d1b6ce12cd2c28c4131c389c00edf45cdb8b0
Daniela Miyasato Goto <daniela.miyasato@live.com>
2022-01-26 16:20:25 -0300

    Requisito 4 - getRelatedEmployees

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,15 @@
 const data = require('../data/zoo_data');
 
+// https://desenvolvimentoparaweb.com/javascript/every-some-find-includes-javascript/
 function isManager(id) {
-  // seu código aqui
+  return data.employees.some((element) => element.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId)) {
+    const subordinados = data.employees.filter((element) => element.managers.includes(managerId)); // pega os funcionários que tem o 'managerId'como gerente
+    return subordinados.map((element) => `${element.firstName} ${element.lastName}`);
+  } throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
