109201b1a084ed8437d414a7e04ce7ea8f85f474
Daniela Miyasato Goto <daniela.miyasato@live.com>
2022-01-26 20:03:57 -0300

    Requisito 5 - countAnimals

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,23 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
-}
+  const objeto = {};
+  // const especie = data.species.find((element) => element.name === animal.specie);
 
+  if (animal === undefined) {
+    data.species.forEach((elemento) => { objeto[elemento.name] = elemento.residents.length; });
+    return objeto;
+  }
+  if (animal.sex === undefined) {
+    return data.species.find((element) => element.name === animal.specie).residents.length;
+  }
+  // if (animal.sex === undefined) {
+  //   return especie.residents.length;
+  // }
+  const sexFilter = (element) => element.sex === animal.sex;
+
+  return data.species.find((element) =>
+    element.name === animal.specie).residents.filter(sexFilter).length;
+}
+console.log(countAnimals({ specie: 'bears' }));
 module.exports = countAnimals;
