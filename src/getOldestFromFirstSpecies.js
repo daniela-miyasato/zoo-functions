9d4ba3485dd082d67612afe9186b69fcd9fc449d
Daniela Miyasato Goto <daniela.miyasato@live.com>
2022-01-27 17:15:42 -0300

    Requisito 9 - getOldestFromFirstSpecies

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -4,8 +4,9 @@ function getOldestFromFirstSpecies(id) {
   const staff = data.employees.find((element) => element.id.includes(id)); // pega o objeto funcionário
   const firstAnimal = staff.responsibleFor[0]; // acha o primeiro animal
   const infoAnimal = data.species.find((element) => element.id === firstAnimal); // pega o objeto animal
-  // const animal = infoAnimal.residents.reduce((ant, atu) => (ant.age > atu.age) ? ant : atu);
-  const animal = infoAnimal.residents.sort((a, b) => b.age - a.age)[0];
+  const animal = infoAnimal.residents.reduce((ant, atu) => (ant.age > atu.age ? ant : atu));
+  // const animal = infoAnimal.residents.sort((a, b) => b.age - a.age)[0];
+  console.log(animal);
   return Object.values(animal);
 }
 
