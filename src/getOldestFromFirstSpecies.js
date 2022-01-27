ddddc0b925499f3fa8628113784711c126f77b68
Daniela Miyasato Goto <daniela.miyasato@live.com>
2022-01-27 17:25:27 -0300

    updating

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -4,10 +4,10 @@ function getOldestFromFirstSpecies(id) {
   const staff = data.employees.find((element) => element.id.includes(id)); // pega o objeto funcionário
   const firstAnimal = staff.responsibleFor[0]; // acha o primeiro animal
   const infoAnimal = data.species.find((element) => element.id === firstAnimal); // pega o objeto animal
-  const animal = infoAnimal.residents.reduce((ant, atu) => (ant.age > atu.age ? ant : atu));
-  // const animal = infoAnimal.residents.sort((a, b) => b.age - a.age)[0];
-  console.log(animal);
-  return Object.values(animal);
+  // const animal = infoAnimal.residents.reduce((ant, atu) => (ant.age > atu.age ? ant : atu)); // tava dando bug, mas arrumei. (tbm funciona)
+  const animal = infoAnimal.residents.sort((a, b) => b.age - a.age)[0];
+  console.log(animal); // ordena (decrescente) e pega o índice 0 que seria 'age' de maior valor
+  return Object.values(animal); // retorna somente os valores
 }
 
 console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
